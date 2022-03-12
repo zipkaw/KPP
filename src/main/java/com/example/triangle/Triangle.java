774018e5cd991cef2c1ca2419a4047de5d9dcf9b
zipkaw <sssinkd4@gmail.com>
2022-03-13 01:15:16 +0300

    git commit

---
+++ src/main/java/com/example/triangle/Triangle.java
@@ -0,0 +1,12 @@
+package com.example.triangle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Triangle {
+    public static void main(String[] args) {
+        SpringApplication.run(Triangle.class, args);
+    }
+
+}
